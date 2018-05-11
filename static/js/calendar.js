5c50db701cf2193ae0d5a354e5d89ebec0754dd5
musta <mustakadem@gmail.com>
2018-05-11 13:00:18 +0200

    mejoras de visualizacion

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,28 +1,28 @@
-$(function () {
-   $('#calendar').fullCalendar({
-       headers:{
 
-       },
-       navLinks:true,
-       editable:false,
-       eventLimit:true,
-       events: 'http://127.0.0.1:8000/home/events/',
-       eventClick:function (calEvent, jsEvent, view) {
-           alert(calEvent.start);
-       }
-       //     function (start, end, timezone, callback) {
-       //
-       //  $.ajax({
-       //    url: 'home/events/',
-       //    type: 'GET',
-       //    dataType: 'json',
-       //    success: function (data) {
-       //          $.each(data, function (index, item) {
-       //              start = item.date_entry;
-       //              end = item.date_departure;
-       //          });
-       //    }
-       //  });
-       // }
-   });
-});
+    $(function () {
+        $('#calendar').fullCalendar({
+            header: {left: 'title  month,'},
+            navLinks: true,
+            editable: false,
+            eventLimit: true,
+            events: 'http://127.0.0.1:8000/home/events/',
+            eventClick: function (calEvent, jsEvent, view) {
+                alert(calEvent.start);
+            },
+            timeFormat: 'H:mm'
+            //     function (start, end, timezone, callback) {
+            //
+            //  $.ajax({
+            //    url: 'home/events/',
+            //    type: 'GET',
+            //    dataType: 'json',
+            //    success: function (data) {
+            //          $.each(data, function (index, item) {
+            //              start = item.date_entry;
+            //              end = item.date_departure;
+            //          });
+            //    }
+            //  });
+            // }
+        });
+    });
