82dc775e9116751ebc32cc667e95aa33fe1a2e43
musta <mustakadem@gmail.com>
2018-05-15 12:50:44 +0200

    mejoras de los metodos ya que no filtran bien las reservas

--- room/views.py
+++ room/views.py
@@ -3,16 +3,20 @@ from __future__ import unicode_literals
 
 from datetime import datetime
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
+from rest_framework import permissions, generics
+from rest_framework.decorators import permission_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from room.serializers import BookingSerializer
 from .models import Room, Booking
 from .forms import NewBooking
-from django.contrib import messages
-# Create your views here.
 
 
 def index(request):
-
     rooms = Room.objects.all()
     return render(request, 'layout.html', {'rooms': rooms})
 
@@ -21,8 +25,11 @@ def detail(request, room_id):
     return HttpResponse("estas en la sala %s." % room_id)
 
 
+@login_required
 def reserva(request, room_id):
+
     room = get_object_or_404(Room, pk=room_id)
+
     if request.method == 'POST':
         form = NewBooking(request.POST)
         if form.is_valid():
@@ -33,16 +40,18 @@ def reserva(request, room_id):
                     date_depature = dates(end_time, request.POST['start'])
                     date_entry = dates(start_time, request.POST['start'])
                     if Booking.objects.filter(room=room, start__gte=date_entry, end__lte=date_depature).exists():
-                        form.add_error(None, "Hay una reserva en ese tramo horario")
+                        form.add_error(None, "Hay una reserva en ese tramo horario en esta sala")
                         return render(request, 'reservate.html', {'room': room, 'form': form})
                     variance = date_depature - date_entry
-                    r = Booking(title=request.POST['title'], start=date_entry, end=date_depature, start_time=start_time, end_time=end_time, room=room, user=request.user)
+                    r = Booking(title=request.POST['title'], start=date_entry, end=date_depature, start_time=start_time,
+                                end_time=end_time, room=room, user=request.user)
+                    r.save()
+                    return HttpResponseRedirect('/')
             else:
-                r = Booking(title=request.POST['title'], start=request.POST['start'], end=request.POST['start'], room=room, user=request.user)
-
-            r.save()
-
-            return HttpResponseRedirect('/')
+                r = Booking(title=request.POST['title'], start=request.POST['start'], end=request.POST['start'],
+                            room=room, user=request.user)
+                r.save()
+                return HttpResponseRedirect('/')
 
     else:
         form = NewBooking()
@@ -55,3 +64,24 @@ def dates(hours, day):
     day = day + " " + hours
     return datetime.strptime(day, "%Y-%m-%d %H:%M:%S")
 
+
+@permission_classes((permissions.AllowAny,))
+class EventsList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class EventsDetail(APIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def get_object(self, pk):
+        try:
+            return Booking.objects.get(pk=pk)
+        except Booking.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        booking = self.get_object(pk)
+        serializer = BookingSerializer(booking)
+        return Response(serializer.data)
