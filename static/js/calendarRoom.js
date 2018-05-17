4df61d8aeb2c6f8b84f8cf19d4cf3dda1166740e
musta <mustakadem@gmail.com>
2018-05-17 13:33:58 +0200

    se muestran los eventos de la sala

--- static/js/calendarRoom.js
+++ static/js/calendarRoom.js
@@ -1,6 +1,35 @@
 $(function () {
+
+let id_room = $("#valueRoom").val();
+
 $("#calendar").fullCalendar({
     height: 600,
-    defaultView:'agendaWeek',
+    navLinks: true,
+    editable: false,
+    eventLimit: true,
+    timeFormat: 'H:mm',
+    events: function (start, end, timezone, callback) {
+
+             $.ajax({
+               url: 'http://127.0.0.1:8000/rooms/'+ id_room +'/',
+               type: 'GET',
+               dataType: 'json',
+               success: function (data) {
+                   let events = [];
+                     $.each(data, function (index, item) {
+                        if (index === 'bookings'){
+                            for(let booking of item){
+                                events.push({
+                                   title: booking.title,
+                                    start: booking.start,
+                                    end: booking.end,
+                                });
+                            }
+                        }
+                     });
+                     callback(events);
+               }
+             });
+            }
 });
 });
